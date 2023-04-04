c8124f5746476d415cac207ee103da049b8c09d9
Ryunosuke1114 <ryunosuke1114@gmail.com>
2023-04-04 18:42:30 +0900

    fix: mainエラー解消

--- main.go
+++ main.go
@@ -1,32 +1,35 @@
 package main
 
 import (
+	"log"
 	"main/handler"
 	"main/infra"
-	"log"
 	"main/service"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	engine := infra.DBInit() 
+	engine := infra.DBInit()
 	factory := service.NewService(engine)
 
-	defer func(){
+	defer func() {
 		log.Println("engine closed")
 		engine.Close()
 	}()
 
 	g := gin.Default()
 
+	g.Use(service.ServiceFactoryMiddleware(factory))
+
 	routes := g.Group("/v1")
 
 	{
-		routes.POST("/users" , handler.Create)
-		routes.GET("/users" , handler.GetAll)
+		routes.POST("/users", handler.Create)
+		routes.GET("/users", handler.GetAll)
 		routes.GET("/users/:user-id", handler.GetOne)
-		routes.PUT("/users/:user-id" , handler.Update)
-		routes.DELETE("/users/:user-id" , handler.Delete)
+		routes.PUT("/users/:user-id", handler.Update)
+		routes.DELETE("/users/:user-id", handler.Delete)
 	}
 
 	g.Run(":3000")
